7be037358e4c50cc840d035b2867053c70484b24
R <rqou@berkeley.edu>
2023-10-21 22:51:35 +0100

    work on circular buffer?

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,8 @@ struct SlidingWindowBuf<
     const HASH_BITS: u64,
 > {
     buf: [u8; TOT_BUF_SZ],
-    lookahead_valid_sz: usize,
+    rpos: usize,
+    wpos: usize,
 }
 
 impl<
@@ -24,7 +25,8 @@ impl<
 
         Self {
             buf: [0; TOT_BUF_SZ],
-            lookahead_valid_sz: 0,
+            rpos: 0,
+            wpos: 0,
         }
     }
 
@@ -47,7 +49,17 @@ impl<
             inp: None,
         }
     }
+
+    fn lookahead_valid_sz(&self) -> usize {
+        if self.wpos >= self.rpos {
+            self.wpos - self.rpos
+        } else {
+            self.wpos + TOT_BUF_SZ - self.rpos
+        }
+    }
 }
+
+struct SpanSet<'a>(&'a [u8], Option<&'a [u8]>, Option<&'a [u8]>);
 
 struct SlidingWindow<
     'a,
@@ -81,16 +93,16 @@ impl<
 {
     fn get_hash(&self) -> u64 {
         let mut tmp_buf = [0; MIN_MATCH];
-        let bytes_to_hash = if self.buf.lookahead_valid_sz >= MIN_MATCH {
-            &self.buf.buf[LOOKBACK_SZ..LOOKBACK_SZ + MIN_MATCH]
-        } else if self.buf.lookahead_valid_sz == 0 {
+        let lookahead_valid_sz = self.buf.lookahead_valid_sz();
+        let bytes_to_hash = if lookahead_valid_sz >= MIN_MATCH {
+            &self.buf.buf[self.buf.rpos..self.buf.rpos + MIN_MATCH]
+        } else if lookahead_valid_sz == 0 {
             &self.inp.unwrap()[..MIN_MATCH]
         } else {
-            tmp_buf[..self.buf.lookahead_valid_sz].copy_from_slice(
-                &self.buf.buf[LOOKBACK_SZ..LOOKBACK_SZ + self.buf.lookahead_valid_sz],
-            );
-            tmp_buf[self.buf.lookahead_valid_sz..]
-                .copy_from_slice(&self.inp.unwrap()[..MIN_MATCH - self.buf.lookahead_valid_sz]);
+            tmp_buf[..lookahead_valid_sz]
+                .copy_from_slice(&self.buf.buf[self.buf.rpos..self.buf.rpos + lookahead_valid_sz]);
+            tmp_buf[lookahead_valid_sz..]
+                .copy_from_slice(&self.inp.unwrap()[..MIN_MATCH - lookahead_valid_sz]);
             &tmp_buf
         };
 
@@ -104,6 +116,15 @@ impl<
 
         hash & ((1 << HASH_BITS) - 1)
     }
+
+    fn tot_ahead(&self) -> usize {
+        self.buf.lookahead_valid_sz() + self.inp.map_or(0, |x| x.len())
+    }
+
+    fn roll_window(&mut self, bytes: usize) {
+        let tot_ahead = self.tot_ahead();
+        assert!(bytes <= tot_ahead);
+    }
 }
 
 #[cfg(test)]
@@ -114,10 +135,10 @@ mod tests {
     fn hash_all_buf() {
         let mut buf: SlidingWindowBuf<1024, 256, { 1024 + 256 }, 3, 512, 15> =
             SlidingWindowBuf::new();
-        buf.buf[1024] = 0x12;
-        buf.buf[1025] = 0x34;
-        buf.buf[1026] = 0x56;
-        buf.lookahead_valid_sz = 3;
+        buf.buf[0] = 0x12;
+        buf.buf[1] = 0x34;
+        buf.buf[2] = 0x56;
+        buf.wpos = 3;
         let hash = buf.flush().get_hash();
         assert_eq!(hash, ((0x12 << 10) ^ (0x34 << 5) ^ 0x56) & 0x7FFF);
     }
@@ -133,8 +154,8 @@ mod tests {
     fn hash_split() {
         let mut buf: SlidingWindowBuf<1024, 256, { 1024 + 256 }, 3, 512, 15> =
             SlidingWindowBuf::new();
-        buf.buf[1024] = 0xde;
-        buf.lookahead_valid_sz = 1;
+        buf.buf[0] = 0xde;
+        buf.wpos = 1;
         let hash = buf.add_inp(&[0xf0, 0x12]).get_hash();
         assert_eq!(hash, ((0xde << 10) ^ (0xf0 << 5) ^ 0x12) & 0x7FFF);
     }
@@ -147,5 +168,17 @@ mod tests {
             hash,
             ((0xde << 12) ^ (0xad << 8) ^ (0xfa << 4) ^ 0xce) & 0x7FFF
         );
+    }
+
+    #[test]
+    fn hash_advanced_rpos() {
+        let mut buf: SlidingWindowBuf<1024, 256, { 1024 + 256 }, 3, 512, 15> =
+            SlidingWindowBuf::new();
+        buf.buf[0] = 0x12;
+        buf.buf[1] = 0x34;
+        buf.buf[2] = 0x56;
+        buf.wpos = 3;
+        let hash = buf.flush().get_hash();
+        assert_eq!(hash, ((0x12 << 10) ^ (0x34 << 5) ^ 0x56) & 0x7FFF);
     }
 }
